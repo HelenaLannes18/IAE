eb64adbcd4d5325cc3e5d88a30a9483a3a770405
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2026-09-22 11:27:13 -0300

    feat: Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 // Configurando a fonte Inter
@@ -43,6 +44,16 @@ export default function RootLayout({
     >
       {/* Aplicando a classe da fonte Inter no body */}
       <body className={`${inter.className} min-h-full flex flex-col bg-[#f5f4f0] text-slate-900`}>
+        {/* Google tag (gtag.js) */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-SH5239LSZF" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SH5239LSZF');
+          `}
+        </Script>
         {children}
         <Analytics />
       </body>
